28a78c6fec69cf167abd5722cb69d5e688859c3e
alihamza1221 <alihamza77065@gmail.com>
2025-12-12 15:49:59 +0500

    friday trade setup in config

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 # Global Trading Configuration
-ORDER_QUANTITY = 0 # refers to $ amount per trade
+ORDER_QUANTITY = 300 # refers to $ amount per trade
 TIMEZONE = 'US/Eastern'  
 
 # ENTRY: Friday pre-market
 #0 = Monday, 1=Tuesday, 2=Wednesday, 3=Thursday, 4=Friday, 5=Saturday, 6=Sunday
 ENTRY_DAY = 4  # (4=Friday)
-ENTRY_TIME_HOUR = 5
-ENTRY_TIME_MINUTE = 26
-ENTRY_TIME_SECOND = 0
+ENTRY_TIME_HOUR = 19
+ENTRY_TIME_MINUTE = 58
+ENTRY_TIME_SECOND = 45
 
 # EXIT: Monday Morning
-EXIT_DAY = 4 # Monday (0=Monday)
-EXIT_TIME_HOUR = 5
-EXIT_TIME_MINUTE = 30
+EXIT_DAY = 0 # Monday (0=Monday)
+EXIT_TIME_HOUR = 4
+EXIT_TIME_MINUTE = 1
 EXIT_TIME_SECOND = 0
 
 # IB Gateway Connection
